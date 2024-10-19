2605921cf0ff10ac4a9f5a9911dd7b855a384eed
Thor777666 <fel777666@gmail.com>
2024-10-19 20:30:34 +0300

    задачи на массивы

---
+++ lesson2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 5;
+int[] arr = new int[n];
+int i = 0;
+while(i < n){
+    arr[i] = i + 1;
+    Console.Write(arr[i] + " ");
+    i += 1;
+}
+
+Console.WriteLine();
+
+int[] arr2 = {5, 10, 4, 3, 11, 20, 13, 6, 17, 8};
+int j = 0;
+while (j < arr2.Length){
+    if (arr2[j] % 2 == 0){
+         Console.Write(arr2[j] + " ");
+    }
+    j += 1;
+}
+
