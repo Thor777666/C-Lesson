00bc2f1b719172a34b2a4692c28c355560a84506
Thor777666 <fel777666@gmail.com>
2024-10-19 21:07:35 +0300

    новые задачи с циклами

--- lesson2/Task2/Program.cs
+++ lesson2/Task2/Program.cs
@@ -4,6 +4,8 @@ for(int i = 0; i < arr.Length; i += 1){
 }
 
 Console.WriteLine();
+////////////////////////////////////////////////////////////////////////////
+
 
 int n = 100;
 int sum = 0;
@@ -13,7 +15,19 @@ for(int i = 0; i < n; i += 1){
 Console.Write(sum);
 
 Console.WriteLine();
+////////////////////////////////////////////////////////////////////////////
 
 foreach(int e in arr){
     Console.Write(e + " ");
 }
+
+Console.WriteLine();
+////////////////////////////////////////////////////////////////////////////
+
+int max = arr[0];
+for(int j = 0; j < arr.Length; j++){
+    if(arr[j] > max){
+        max = arr[j];
+    }
+}
+Console.Write(max);
