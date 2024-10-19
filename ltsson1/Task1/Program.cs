3caf49bf217712322afd1f9f36a6447a04908690
Thor777666 <fel777666@gmail.com>
2024-10-19 17:49:45 +0300

    Исправлены ошибки

--- ltsson1/Task1/Program.cs
+++ ltsson1/Task1/Program.cs
@@ -2,23 +2,26 @@
 
 int n = 30;
 int i = 1;
- while(i <= n){
-    Console.Write(i);
-    Console.Write(" ");
-    i += 1;
- }
- 
+while (i <= n)
+{
+   Console.Write(i);
+   Console.Write(" ");
+   i += 1;
+}
 
- Console.WriteLine("Егор лох");
+
+Console.WriteLine("Егор лох");
 
 int a = 10;
 int b = 6;
 int c = 20;
 int max = a;
-if (b > a){
- max = b
- }
-if (c > max){
- max = c
- }
- Console.WriteLine(max);
+if (b > a)
+{
+   max = b;
+}
+if (c > max)
+{
+   max = c;
+}
+Console.WriteLine(max);
