830e808c3f0717df192f5791df3b259168be9a0a
Thor777666 <fel777666@gmail.com>
2024-10-19 20:54:15 +0300

    задачи с циклом for

---
+++ lesson2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = {5, 10, 12, 3, 5};
+for(int i = 0; i < arr.Length; i += 1){
+    Console.Write(arr[i] + " ");
+}
+
+Console.WriteLine();
+
+int n = 100;
+int sum = 0;
+for(int i = 0; i < n; i += 1){
+    sum += i;
+}
+Console.Write(sum);
+
+Console.WriteLine();
+
+foreach(int e in arr){
+    Console.Write(e + " ");
+}
