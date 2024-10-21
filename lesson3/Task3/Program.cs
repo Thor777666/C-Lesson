8139b13af9e38b0aa75db0512d5ed85bcaba853b
Thor777666 <fel777666@gmail.com>
2024-10-21 18:02:14 +0300

    Изучил ввод в консоли

---
+++ lesson3/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int size = 5;
+int[] arr = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine("Введите элемент масссива: ");
+    string input = Console.ReadLine();
+    arr[i] = Convert.ToInt32(input);
+    i++;
+}
+
+foreach(int n in arr){
+    Console.Write(n + " ");
+}
