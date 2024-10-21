6cadf782222903ce35ca0696df3a88b603c8d3d8
Thor777666 <fel777666@gmail.com>
2024-10-21 15:31:12 +0300

    Функция по обнулению чётных чисел в массиве

---
+++ lesson3/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿void ZeroEvenEl(int[] arr){
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i] % 2 == 0){
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArr(int[] arr){
+    foreach(int el in arr){
+        Console.Write(el + " ");
+    }
+}
+
+int[] array = {2, 5, 10, 3, 9, 12};
+ZeroEvenEl(array);
+PrintArr(array);
