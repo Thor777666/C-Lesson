28faed10646c1f8d07fefe1ce7047e136331d186
Thor777666 <fel777666@gmail.com>
2024-10-21 14:49:15 +0300

    Изучение функций

---
+++ lesson3/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalcForm(int a, int b, int c, int d){
+    double numen = a + b;
+    int denumen = c * d;
+    double result = numen / denumen;
+    return result;
+}
+
+double result = CalcForm(1, 2, 3, 4);
+Console.WriteLine(result);
